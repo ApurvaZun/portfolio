3e06433b26c7acee2429bc78960edacc1e541ccf
ApurvaZun <apu.zunzunwala@gmail.com>
2021-03-16 11:03:29 +0100

    add Grid to layout

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
+import Grid from '@material-ui/core/Grid';
 
 function App() {
-    const [state, setState] = useState("CLICK ME");
 
-    return <button onClick={() => setState("CLICKED")}>{state}</button>;
+    return (
+        <div>
+            <Grid container spacing={1}>
+                <Grid item xs={6}>
+                    block 1
+                </Grid>
+                <Grid item xs={6}>
+                    block 2
+                </Grid>
+            </Grid>
+        </div>
+        
+    )
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
