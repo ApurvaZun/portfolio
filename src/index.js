0d0d6f578d6b57cfeed9bf7aefcb36fe7aebfe7a
ApurvaZun <apu.zunzunwala@gmail.com>
2021-03-16 13:59:16 +0100

    add portfolio info in json

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Grid from '@material-ui/core/Grid';
 
-function App() {
+import UserInfo from "./components/userInfo";
+import "./index.scss";
+
+const App = () => {
 
     return (
         <div>
             <Grid container spacing={1}>
                 <Grid item xs={6}>
-                    block 1
+                    <UserInfo />
                 </Grid>
                 <Grid item xs={6}>
                     block 2
