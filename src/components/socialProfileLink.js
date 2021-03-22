980e8a9ffcb4986f40604dafc5e245446d09f29b
ApurvaZun <apu.zunzunwala@gmail.com>
2021-03-22 14:10:10 +0100

    fix: github link

--- src/components/socialProfileLink.js
+++ src/components/socialProfileLink.js
@@ -9,14 +9,14 @@ const SocialProfileLinks = () => {
     return(
         <ul>
             <li>
-                <a href={user.LinkedinURL} rel="noreferrer" target="_blank" className="item-social-icon">
+                <a href={user[0].LinkedinURL} rel="noreferrer" target="_blank" className="item-social-icon">
                     <LinkedInIcon fontSize="small" />
                     <span>LinkedIn</span>
                     <OpenInNewIcon fontSize="small" />
                 </a>
             </li>
             <li>
-                <a href={user.GithubURL} rel="noreferrer" target="_blank" className="item-social-icon">
+                <a href={user[0].GithubURL} rel="noreferrer" target="_blank" className="item-social-icon">
                     <GitHubIcon fontSize="small" />
                     <span>GitHub</span>
                     <OpenInNewIcon fontSize="small" />
